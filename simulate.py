adcafece21fcb340eace9f6458a0566b1295067d
John Lackey <jlackey@uvm.edu>
2025-01-16 22:24:44 -0500

    added printing and sleeping

--- simulate.py
+++ simulate.py
@@ -1,3 +1,15 @@
 import pybullet as p
+import time
+
+SLEEP_CONSTANT = 1/60
+
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(SLEEP_CONSTANT)
+    
+
 p.disconnect()
