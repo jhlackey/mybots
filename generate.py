fdbdee2383bd8512b27c61240c3998396279aaba
John Lackey <jlackey@uvm.edu>
2025-01-27 16:09:36 -0500

    generate 5x5 of 10 tower... gory approach

--- generate.py
+++ generate.py
@@ -13,12 +13,23 @@ z = .5
 pyrosim.Start_SDF("boxes.sdf")
 
 # stores a box with initial position x=0, y=0, z=0.5, and length, width and height all equal to 1 meter, in box.sdf.
+
 # pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
-for i in range(10):
-    # i *= .5
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z + i] , size=[length,width,height])
+for x in range(5):
+    length = 1
+    width = 1
+    height = 1
+    for y in range(5):
+        length = 1
+        width = 1
+        height = 1
+        for i in range(10):
+            # i *= .5
+            length = length * 0.9
+            width = width * 0.9
+            height = height * 0.9
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z + (i * .9) ] , size=[length,width,height])
 
-# pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length,width,height])
 
 pyrosim.End()
