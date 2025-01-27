8873f9da96d792fdc7bc147a83566fa99f15c2ed
John Lackey <jlackey@uvm.edu>
2025-01-27 13:31:28 -0500

    Added floor

--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 SLEEP_CONSTANT = 1/60
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+p.setGravity(0,0,-9.8) #responsible for determining what forces exist in our world. The first, most obvious one to add is gravity. 
+planeId = p.loadURDF("plane.urdf") # add floor
 p.loadSDF("box.sdf") #tells pybullet to read in the world described in box.sdf.
 for i in range(1000):
     print(i)
