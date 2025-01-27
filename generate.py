41db979aae6c3ed941ce03fb49afeaf236157cce
John Lackey <jlackey@uvm.edu>
2025-01-27 13:41:10 -0500

    added length, width, height, x, y, z, and set z to 1.5

--- generate.py
+++ generate.py
@@ -1,9 +1,18 @@
 import pyrosim.pyrosim as pyrosim
 
+# constants
+length = 1
+width = 2
+height = 3
+
+x = 0  
+y = 0
+z = 1.5
+
 #  tell pyrosim the name of the file where information about the world you're about to create should be stored. 
 pyrosim.Start_SDF("box.sdf")
 
 # stores a box with initial position x=0, y=0, z=0.5, and length, width and height all equal to 1 meter, in box.sdf.
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()
