d4803cb78b37cd19bb1a4a801b70b06630f571de
John Lackey <jlackey@uvm.edu>
2025-01-27 09:38:50 -0500

    removed .git from pyrosim

--- simulate.py
+++ simulate.py
@@ -4,12 +4,11 @@ import time
 SLEEP_CONSTANT = 1/60
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 for i in range(1000):
     print(i)
     p.stepSimulation()
     time.sleep(SLEEP_CONSTANT)
     
-
 p.disconnect()
