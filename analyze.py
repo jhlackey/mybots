a38982dd586e6b3909cf0ec384ff3a076826c777
jhlackey <jlackey@uvm.edu>
2025-02-17 21:56:13 -0500

    Scaled data

--- analyze.py
+++ analyze.py
@@ -1,6 +1,20 @@
 import numpy 
 import matplotlib.pyplot as pyplot
+import os
 
-backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
-pyplot.plot(backLegSensorValues)
+# backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+# frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+targetAngles = numpy.load('data/targetAngles.npy')
+pyplot.plot(targetAngles)
+
+# pyplot.plot(backLegSensorValues, label='BackLeg', lw=3)
+# pyplot.plot(frontLegSensorValues, label='FrontLeg',)
+
+pyplot.ylabel('Value in Radians')
+pyplot.xlabel('Steps')
+pyplot.title('Motor Commands')
+pyplot.legend()
 pyplot.show()
+
+
+pyplot.savefig(os.path.join('data', 'targetAngles.png'))
