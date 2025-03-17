19098cf373e56c3d9edc6cf736c053f6ee17d205
jhlackey <jlackey@uvm.edu>
2025-03-17 19:20:37 -0400

    modified generate.py and created search.py

--- generate.py
+++ generate.py
@@ -1,3 +1,6 @@
+# from pybullet_envs.deep_mimic.env.testLaikago import jointName
+import random
+
 import pyrosim.pyrosim as pyrosim
 
 # constants
@@ -37,17 +40,28 @@ def Generate_Body(): # step 5, renamed Create_Robot()
 
 def Generate_Brain(): # step 5, renamed Create_Robot()
     pyrosim.Start_NeuralNetwork("brain.nndf")
+    motorNames = [3,4] # replace with indices
+    sensorNames = [0,1,2]
+
+    # Inside generate, create two nested for loops.
+    # The outer loop should iterate over the names of the three sensor neurons.
+    # The inner loop should iterate over each of the two motor neurons.
+
     pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
     pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
     pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=5, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.0)
+    for sensor in sensorNames:
+        for motor in motorNames:
+            pyrosim.Send_Synapse(sourceNeuronName=sensor, targetNeuronName=motor, weight=random.uniform(-1,1))
+
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=5, weight=1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=1.0)
+    # pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1.0)
 
     pyrosim.End()
 
